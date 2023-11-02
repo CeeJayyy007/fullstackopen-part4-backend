ba3fe134b9c45982ada06849f1b5d869b49a0f77
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-02 06:11:49 -0400

    complete 4.4: unit tests step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+// define function totalLikes
+// eslint-disable-next-line no-unused-vars
+const totalLikes = (blogs) => {
+  // receive an array of blogs as a parameter
+  // return the total sum of likes in all of the blog posts
+  // if the array is empty, return 0
+
+  const sumOfLikes = (sum, blog) => {
+    return sum + blog.likes;
+  };
+
+  return blogs.reduce(sumOfLikes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
