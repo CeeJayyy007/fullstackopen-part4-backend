d995839e526eacba8560b7e9e8be05c1e2591ce7
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-02 09:49:53 -0400

    complete 4.7: helper functions and units tests step 5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -106,8 +106,90 @@ describe("favourite blog", () => {
     likes: 5,
   };
 
-  test("from the list of blogs is", () => {
+  test("from the list of blogs", () => {
     const result = listHelper.favoriteBlog(biggerList);
     expect(result).toEqual(favouriteBlogData);
+  });
+});
+
+describe("most blogs", () => {
+  // initialize an array with multiple blogs
+  const biggerList = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b512c36234d17f8",
+      title: "New Blog Post",
+      author: "Edgar wright",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 2,
+      __v: 0,
+    },
+    {
+      _id: "6f322aa71b54a676234d17f8",
+      title: "Third Blog Post",
+      author: "Edgar wright",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  // favourite blog data
+  const favouriteBlogData = {
+    author: "Edgar wright",
+    blogs: 2,
+  };
+
+  test("by a single author", () => {
+    const result = listHelper.mostBlogs(biggerList);
+    expect(result).toEqual(favouriteBlogData);
+  });
+});
+
+describe("most likes", () => {
+  // initialize an array with multiple blogs
+  const biggerList = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b512c36234d17f8",
+      title: "New Blog Post",
+      author: "Edgar wright",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 2,
+      __v: 0,
+    },
+    {
+      _id: "6f322aa71b54a676234d17f8",
+      title: "Third Blog Post",
+      author: "Edgar wright",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 1,
+      __v: 0,
+    },
+  ];
+
+  // favourite blog data
+  const mostLikesBlogData = {
+    author: "Edsger W. Dijkstra",
+    likes: 5,
+  };
+
+  test("by a single author", () => {
+    const result = listHelper.mostLikes(biggerList);
+    expect(result).toEqual(mostLikesBlogData);
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   // receive an array of blogs as a parameter
@@ -34,9 +36,50 @@ const favoriteBlog = (blogs) => {
   return blogData;
 };
 
+// define function mostBlogs
+const mostBlogs = (blogs) => {
+  // count the occurences of each author
+  const authorCounts = _.countBy(blogs, "author");
+
+  // get the name of the author with the most blogs
+  const maxAuthorName = _.maxBy(
+    Object.keys(authorCounts),
+    (author) => authorCounts[author]
+  );
+
+  // create an object with the author name and the number of blogs
+  const authorBlogCount = {
+    author: maxAuthorName,
+    blogs: authorCounts[maxAuthorName],
+  };
+
+  return authorBlogCount;
+};
+
+// define function mostLikes
+const mostLikes = (blogs) => {
+  // Group blogs by author and sum likes
+  const likesByAuthor = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes;
+    return acc;
+  }, {});
+
+  // Find the author with the most likes
+  const mostLikedAuthor = Object.entries(likesByAuthor).reduce(
+    (max, [author, likes]) => {
+      return likes > max.likes ? { author, likes } : max;
+    },
+    { author: "", likes: 0 }
+  );
+
+  return mostLikedAuthor;
+};
+
 // export the functions
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
