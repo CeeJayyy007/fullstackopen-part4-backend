b6b7b14884f2ce05e538d180c62e9fee3646b4a0
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-03 04:23:58 -0400

    complete 4.10: blog list tests step 3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -33,3 +33,24 @@ test("verify that the unique identifier propert is named id", async () => {
 
   expect(response.body[0].id).toBeDefined();
 });
+
+test("a valid blog post can be added", async () => {
+  const newBlog = {
+    title: "New blog post for testing",
+    author: "Kelvin Philips",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 3,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  const title = blogsAtEnd.map((b) => b.title);
+
+  expect(title).toContain("New blog post for testing");
+}, 10000);
