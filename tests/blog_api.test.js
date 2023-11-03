06797e68dd853a49c2f826bc0272f2eaa0ba9343
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-03 05:14:54 -0400

    update blogs tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,111 +11,117 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there is initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test("verify that the unique identifier property is named id", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body[0].id).toBeDefined();
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+describe("addition of a new blog post", () => {
+  test("a valid blog post can be added", async () => {
+    const newBlog = {
+      title: "New blog post for testing",
+      author: "Kelvin Philips",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 3,
+    };
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const title = blogsAtEnd.map((b) => b.title);
+
+    expect(title).toContain("New blog post for testing");
+  }, 10000);
+
+  test("likes = 0 if likes property is missing", async () => {
+    const newBlog = {
+      title: "New blog post for testing",
+      author: "Kelvin Philips",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body.likes).toBe(0);
+  });
+
+  test("respond with 400 Bad request if title or url missing", async () => {
+    const newBlog = {
+      author: "Kelvin Philips",
+      likes: 3,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test("update likes of a blog post", async () => {
+    const newBlog = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 6,
+    };
+
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(newBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    const updatedBlog = blogsAtEnd[0];
+
+    expect(updatedBlog.likes).toBe(6);
+  });
 });
 
-test("verify that the unique identifier propert is named id", async () => {
-  const response = await api.get("/api/blogs");
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  expect(response.body[0].id).toBeDefined();
-});
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-test("a valid blog post can be added", async () => {
-  const newBlog = {
-    title: "New blog post for testing",
-    author: "Kelvin Philips",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 3,
-  };
+    const blogsAtEnd = await helper.blogsInDb();
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-  const title = blogsAtEnd.map((b) => b.title);
+    const title = blogsAtEnd.map((b) => b.title);
 
-  expect(title).toContain("New blog post for testing");
-}, 10000);
-
-test("likes = 0 if likes property is missing", async () => {
-  const newBlog = {
-    title: "New blog post for testing",
-    author: "Kelvin Philips",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-  };
-
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  expect(response.body.likes).toBe(0);
-});
-
-test("respond with 400 Bad request if title or url missing", async () => {
-  const newBlog = {
-    author: "Kelvin Philips",
-    likes: 3,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-});
-
-test("succeeds with status code 204 if id is valid", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-  const title = blogsAtEnd.map((b) => b.title);
-
-  expect(title).not.toContain(blogToDelete.title);
-});
-
-test("update likes of a note", async () => {
-  const newBlog = {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 6,
-  };
-
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToUpdate = blogsAtStart[0];
-
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send(newBlog)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  const updatedBlog = blogsAtEnd[0];
-
-  expect(updatedBlog.likes).toBe(6);
+    expect(title).not.toContain(blogToDelete.title);
+  });
 });
 
 afterAll(async () => {
