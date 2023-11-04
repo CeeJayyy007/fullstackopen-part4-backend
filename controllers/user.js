ab71d8ca8167e53607cc22f73b2d5aec7b5bb3a9
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-04 04:30:48 -0400

    add pasword validation to user controller

--- controllers/user.js
+++ controllers/user.js
@@ -11,6 +11,13 @@ const User = require("../models/user");
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  // validate password
+  if (!password || password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "password must be at least 3 characters long" });
+  }
+
   // hash password
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
