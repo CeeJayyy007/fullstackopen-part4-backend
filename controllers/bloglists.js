3af5cbbfb2e28cc00d166956e809439573425371
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-25 20:34:33 -0500

    chore: update variable name

--- controllers/bloglists.js
+++ controllers/bloglists.js
@@ -41,13 +41,13 @@ blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
 blogsRouter.put("/:id", middleware.userExtractor, async (request, response) => {
   const { title, author, url, likes } = request.body;
 
-  const updatedNote = await Blog.findByIdAndUpdate(
+  const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
     { title, author, url, likes },
     { new: true, runValidators: true, context: "query" }
   ).populate("user", { username: 1, name: 1 });
 
-  response.json(updatedNote);
+  response.json(updatedBlog);
 });
 
 // delete blog post
